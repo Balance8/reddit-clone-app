3407efb23469b1c0eb3d09a9a860b4200ca61916
Michael Slocum <mslocum819@gmail.com>
2018-05-23 10:43:37 -0700

    postSchema + backend

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,17 @@
-// Create your Post model here
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema(
+  {
+    title,
+    content,
+    thumbnail_image_url,
+    votes,
+    comments: Array
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Post", postSchema);
